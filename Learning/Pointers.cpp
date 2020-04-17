b3f795ebc91b26a56e99d7e5382b935da3915967
Rubén Guerrero Ramírez <ruben.guerrero.ramirez@outlook.com>
2020-04-17 20:22:17 +0200

    Correction made on comment

--- Learning/Pointers.cpp
+++ Learning/Pointers.cpp
@@ -6,7 +6,7 @@ void Pointers() {
 	int myVar = 8;
 
 	// The following pointer shows the memory address where the value 8 from myVar is stored.
-	void* ptr = &myVar; // ptr has de memory address 0x003fe04 but this pointer cannot be dereferenced as its type is void.
+	void* ptr = &myVar; // ptr has the memory address 0x003fe04 but this pointer cannot be dereferenced as its type is void.
 	LOG(ptr);
 	
 	int* ptr1 = &myVar; // ptr1 stores the same address as ptr, but its value can be changed as is typed as integer.
